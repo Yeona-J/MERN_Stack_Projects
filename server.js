e6234d3aa2bf2e654d19bcbd58989eff858f6369
Yeona-J <yeon.aa.j@gmail.com>
2023-05-04 22:33:07 +0900

    feat: Add MongoDB and Mongoose integration with Express

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Define the connection URI
+const uri = "mongodb+srv://1234567890:1234567890@cluster0.3i3gxco.mongodb.net/?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+async function connect() {
+    try {
+        await mongoose.connect(uri, {
+            useNewURLParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('connected to mongoDB');
+    } catch(error) {
+        console.error('error');
+    }
+}
+
+connect();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
