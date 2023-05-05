c9b4fc19f97514bc90277890ef4d8dafad3b2de4
Yeona-J <yeon.aa.j@gmail.com>
2023-05-06 02:55:24 +0900

    feat: Add input change handlers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,18 +6,26 @@ function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleUsernameChange = (event) => { // ES6
+    setUsername(event.target.value)
+  };
+
+  const handlerPasswordChange = (event) => {
+    setPassword(event.target.value)
+  };
+
   return (
     <div className="App">
     <h1>Welcome to the Game!</h1>
     <form>
       <label>
         Username:
-        <input type="text" />
+        <input type="text" value={username} onChange={handleUsernameChange} />
       </label>
       <br />
       <label>
         Password:
-        <input type="text" />
+        <input type="text" value={password} onChange={handlerPasswordChange} />
       </label>
       <br />
       <button type="submit">Submit</button>
