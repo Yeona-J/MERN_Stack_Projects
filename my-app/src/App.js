5830a3789358f605b081866ca043a81338ea4da9
Yeona-J <yeon.aa.j@gmail.com>
2023-05-06 02:14:35 +0900

    feat: Userform

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Welcome to the Game!</h1>
+    <form>
+      <label>
+        Username:
+        <input type="text" />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="text" />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
     </div>
   );
 }
